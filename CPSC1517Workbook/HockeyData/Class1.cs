7811e93ab452dafe39013b4e0f9c725fde4c1ec3
Anam Abid <97272212+anamabid1@users.noreply.github.com>
2023-09-12 17:21:15 -0600

    Create Workbook Solution

---
+++ CPSC1517Workbook/HockeyData/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HockeyData
+{
+    public class Class1
+    {
+    }
+}
